6b6b3ec05360245f0272f06b161d1d350d291a3d
khw <hannahkhw@platypusoft.com>
2025-01-09 21:07:25 +0900

    first commit

---
+++ src/main/java/kimheonningg/chatgpt_backend/ChatgptBackendApplication.java
@@ -0,0 +1,13 @@
+package kimheonningg.chatgpt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptBackendApplication.class, args);
+	}
+
+}
