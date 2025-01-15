c0e9744c502aa9734fec59373348e0e28bcc2e7a
khw <hannahkhw@platypusoft.com>
2025-01-16 02:05:38 +0900

    add basic structure that uses OpenAI API to chat

---
+++ src/main/java/kimheonningg/chatgpt_backend/data/Answer.java
@@ -0,0 +1,12 @@
+package kimheonningg.chatgpt_backend.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    private String answer;
+}
---
+++ src/main/java/kimheonningg/chatgpt_backend/data/ChatGPTChoice.java
@@ -0,0 +1,15 @@
+package kimheonningg.chatgpt_backend.data;
+
+import java.util.Map;
+
+import io.micrometer.common.lang.Nullable;
+import lombok.Data;
+
+@Data
+public class ChatGPTChoice {
+    private int index;
+    private ChatGPTMessage message;
+    @Nullable
+    private Map<String, double[]> logprobs;
+    private String finish_reason;
+}
