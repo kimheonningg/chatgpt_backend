4448a1fc187d4657e95d55fd3e2df4cd8f1c6c6f
khw <hannahkhw@platypusoft.com>
2025-01-15 16:42:01 +0900

    add test controller

---
+++ src/main/java/kimheonningg/chatgpt_backend/data/Version.java
@@ -0,0 +1,10 @@
+package kimheonningg.chatgpt_backend.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Version {
+    private String version;
+}
